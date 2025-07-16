cd0f85ff06432082e18b6341d24e0a2d9a9dc5be
sun0lee <sun0lee88@gmail.com>
2025-07-16 14:14:46 +0900

    add sql
    sql (data처리는 SQLite가 프로시저를 지원하지 않으므로 파이선에 추가시킬지 고민필요)

--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ from core import API_CONFIG_PATH
 kis_client = KisClient()
 db = DatabaseManager()
 
-# 종목코드
-# code_list = [ "201W09412", "301W09412"]
+# 종목코드 : mst_inst 에서 use_yn ='Y' 인 대상 추출
 inst_list = db.get_inst_list()
 
 # api 설정 : json file에서 관리 : config > api_configs.json
@@ -25,6 +24,9 @@ if __name__ == "__main__":
     # 2 : 선물옵션기간별시세(일/주/월/년)
     # 3 : 선물옵션 분봉조회
     # 4 : 국내선물 기초자산 시세
+    # 공백 : 1~4 실행
 
     manager.save_data()
+    # manager.save_data(job_list=["1"])
+    # manager.save_data(job_list=["2","3"])
     # manager.save_data(job_list=["4"])
